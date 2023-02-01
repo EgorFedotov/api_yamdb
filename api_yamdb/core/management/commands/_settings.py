5c2a366049d5d353264a851082d66a95b637806c
Mikhail Gazetov <m.gazetov@gmail.com>
2023-02-01 16:18:31 +0200

    #1: load from file

---
+++ api_yamdb/core/management/commands/_settings.py
@@ -0,0 +1,11 @@
+from api.models import Category
+
+model_by_filename = {
+    'comments': Comment,
+    'category': Category,
+    'genre': Genre,
+    'genre_title': GenreTitle,
+    'review': Review,
+    'titles': Title,
+    'users': User,
+}
