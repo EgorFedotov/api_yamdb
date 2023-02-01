351303637a51fc4bd46a1374feded185c58d17d8
Mikhail Gazetov <m.gazetov@gmail.com>
2023-02-01 15:13:19 +0200

    #3: add title. Add migrations

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -3,7 +3,7 @@ from django.urls import include, path
 
 from rest_framework import routers
 
-from api.views import CategoryViewSet, GenreViewSet
+from api.views import CategoryViewSet, GenreViewSet, TitleViewSet
 
 
 V1_PATH = 'v1/'
@@ -11,6 +11,7 @@ V1_PATH = 'v1/'
 router_v1 = routers.DefaultRouter()
 router_v1.register(f'{V1_PATH}categories', CategoryViewSet)
 router_v1.register(f'{V1_PATH}genres', GenreViewSet)
+router_v1.register(f'{V1_PATH}titles', TitleViewSet)
 
 
 urlpatterns = [
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets
 
-from reviews.models import Category, Genre
-from .serializers import CategorySerializer, GenreSerializer
+from reviews.models import Category, Genre, Title
+from .serializers import CategorySerializer, GenreSerializer, TitleSerializer
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
@@ -12,3 +12,8 @@ class CategoryViewSet(viewsets.ModelViewSet):
 class GenreViewSet(viewsets.ModelViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    serializer_class = TitleSerializer
+    queryset = Title.objects.all()
