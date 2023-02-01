fd5afb1f2762500231acc95ccce63b829e725a67
Mikhail Gazetov <m.gazetov@gmail.com>
2023-02-01 14:00:18 +0200

    #3: add model

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -9,3 +9,20 @@ class Category(models.Model):
 class Genre(models.Model):
     name = models.CharField('Название', max_length=256)
     slug = models.SlugField(unique=True, max_length=50)
+
+
+class Title(models.Model):
+    name = models.CharField('Название', max_length=256)
+    year = models.PositiveSmallIntegerField()
+    # rating = TODO: review.score 
+    description = models.TextField()
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.DO_NOTHING,
+        related_name='titles',
+    )
+    genre = models.ForeignKey(
+        Genre,
+        on_delete=models.DO_NOTHING,
+        related_name='titles'
+    )
