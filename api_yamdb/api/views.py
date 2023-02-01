b6a4fca9a760b9729f8d01d7bb52d5274b0dd64a
Mikhail Gazetov <m.gazetov@gmail.com>
2023-02-01 15:28:01 +0200

    remove update method from category

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,9 +1,13 @@
-from rest_framework import viewsets
+from rest_framework import mixins, viewsets
 
 from reviews.models import Category
 from .serializers import CategorySerializer
 
 
-class CategoryViewSet(viewsets.ModelViewSet):
+class CategoryViewSet(mixins.CreateModelMixin,
+                      mixins.RetrieveModelMixin,
+                      mixins.DestroyModelMixin,
+                      mixins.ListModelMixin,
+                      viewsets.GenericViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
