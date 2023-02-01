e6cf278011f365609e3e17570dcb0334e9c3177c
Mikhail Gazetov <m.gazetov@gmail.com>
2023-02-01 23:32:43 +0200

    1: fix related field problem

--- api_yamdb/core/management/commands/_settings.py
+++ api_yamdb/core/management/commands/_settings.py
@@ -1,11 +1,12 @@
-from api.models import Category
+from reviews.models import Category, Genre, Title
 
+#TODO:
 model_by_filename = {
-    'comments': Comment,
+    #'comments': Comment,
     'category': Category,
     'genre': Genre,
-    'genre_title': GenreTitle,
-    'review': Review,
+    #'genre_title': GenreTitle,
+    #'review': Review,
     'titles': Title,
-    'users': User,
+    #'users': User,
 }
--- api_yamdb/core/management/commands/loadfromfile.py
+++ api_yamdb/core/management/commands/loadfromfile.py
@@ -1,8 +1,10 @@
 import csv
 import os
 
+from typing import Any, List
+
 from django.conf import settings
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 
 from ._settings import model_by_filename
 
@@ -10,18 +12,43 @@ from ._settings import model_by_filename
 class Command(BaseCommand):
     help = 'Load data from csv files into database.'
 
+    @staticmethod
+    def is_related(object: Any, field: Any) -> bool:
+        """Model field is related to foreign key."""
+        return not hasattr(object, field)
+
+    @staticmethod
+    def add_suffix_for_related(model: Any, keys: List[str]) -> None:
+        """Add id suffix for all related fields."""
+        instance = model()
+        for i, field in enumerate(keys):
+            if Command.is_related(instance, field):
+                keys[i] += "_id"
+
     def handle(self, *args, **options):
+        """Load data from csv files to database."""
         for filename, model in model_by_filename.items():
             filename += '.csv'
             path = os.path.join(settings.STATIC_DATA, filename)
+
             with open(path, encoding='utf-8') as csvfile:
                 reader = csv.reader(csvfile, delimiter=',')
                 keys = next(reader)
+                Command.add_suffix_for_related(model, keys)
+                update, error = False, False
                 for row in reader:
                     kwargs = {key: column for key, column in zip(keys, row)}
-                    model.objects.create(**kwargs)
-                    
+                    object, created = model.objects.update_or_create(**kwargs)
+                    if not object:
+                        error = True
+                    elif not created:
+                        update = True
 
-        self.stdout.write(self.style.ERROR('Not loaded'))
-
-
+                cout = self.stdout.write
+                cerr = self.stderr.write
+                if error:
+                    cerr(self.style.ERROR(f'Not load {model.__name__}'))
+                elif update:
+                    cout(self.style.SUCCESS(f'Update {model.__name__}'))
+                else:
+                    cout(self.style.SUCCESS(f'Created {model.__name__}'))
