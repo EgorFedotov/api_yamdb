29dcb01b54bc6de47e510b1b7e5739a1f528c64e
Mikhail Gazetov <m.gazetov@gmail.com>
2023-02-01 10:25:57 +0200

    #2: fix length to model. Add v1 prefix for path

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -6,11 +6,11 @@ from rest_framework import routers
 from api.views import CategoryViewSet
 
 
-router_v1 = routers.DefaultRouter()
-router_v1.register(r'categories', CategoryViewSet)
-
-
 V1_PATH = 'v1/'
+
+router_v1 = routers.DefaultRouter()
+router_v1.register(f'{V1_PATH}categories', CategoryViewSet)
+
 
 urlpatterns = [
     path('', include(router_v1.urls)),
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField('Название', max_length=255)
-    slug = models.SlugField(unique=True)
+    name = models.CharField('Название', max_length=256)
+    slug = models.SlugField(unique=True, max_length=50)
