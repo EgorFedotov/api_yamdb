6646a2f3e3636f7194211d841f276a8fa68dd662
Mikhail Gazetov <m.gazetov@gmail.com>
2023-02-09 13:21:31 +0200

    fix prefix dj

--- api_yamdb/api/filters.py
+++ api_yamdb/api/filters.py
@@ -1,13 +1,13 @@
-import django_filters as dj_filters
+import django_filters
 
 from reviews.models import Title
 
 
-class TitleFilter(dj_filters.FilterSet):
-    name = dj_filters.CharFilter(field_name='name', lookup_expr='contains')
-    year = dj_filters.NumberFilter(field_name='year')
-    category = dj_filters.CharFilter(field_name='category__slug')
-    genre = dj_filters.CharFilter(field_name='genre__slug')
+class TitleFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(field_name='name', lookup_expr='contains')
+    year = django_filters.NumberFilter(field_name='year')
+    category = django_filters.CharFilter(field_name='category__slug')
+    genre = django_filters.CharFilter(field_name='genre__slug')
 
     class Meta:
         model = Title
