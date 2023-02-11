8aa966d0bdd2187b6e7ce84cfa92ed91757af5a6
Mikhail Gazetov <m.gazetov@gmail.com>
2023-02-11 14:15:56 +0200

    fix views.py

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -32,16 +32,19 @@ def register(request):
     '''Регистрация пользователя.'''
     serializer = RegisterDataSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
-    user, create = User.objects.get_or_create(
-        **serializer.validated_data)
-    if create:
-        confirmation_code = default_token_generator.make_token(user)
-        send_mail(
-            subject='YaMDb registration',
-            message=f'Your confirmation code: {confirmation_code}',
-            from_email=None,
-            recipient_list=[user.email],
+    try:
+        user, create = User.objects.get_or_create(
+            **serializer.validated_data
         )
+    except IntegrityError:
+        raise ValidationError("Неверное имя пользователя или email")
+    confirmation_code = default_token_generator.make_token(user)
+    send_mail(
+        subject='YaMDb registration',
+        message=f'Your confirmation code: {confirmation_code}',
+        from_email=None,
+        recipient_list=[user.email],
+    )
 
     return Response(serializer.data, status=status.HTTP_200_OK)
 
@@ -86,15 +89,19 @@ class UserViewSet(ModelViewSet):
         permission_classes=[permissions.IsAuthenticated],
     )
     def users_own_profile(self, request):
-        serializer = self.get_serializer(
+        serializer = UserSerializer(
             request.user,
-            data=request.data if request.method != "GET" else {},
+            data=request.data,
             partial=True
         )
-        serializer.is_valid(raise_exception=True)
+        if request.method == "GET":
+            serializer.is_valid(raise_exception=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         if request.method == "PATCH":
+            serializer.is_valid(raise_exception=True)
             serializer.save(role=request.user.role)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
 
 class CategoryViewSet(AdminControlSlugViewSet):
